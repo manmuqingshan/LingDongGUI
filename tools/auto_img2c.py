6d1933a6bddc14a7c50b56b8e2bbd6d4d8061830
gzbkey <59935554@qq.com>
2025-05-14 17:21:09 +0800

    更新脚本

--- tools/auto_img2c.py
+++ tools/auto_img2c.py
@@ -1,8 +1,9 @@
 import os
+import yaml
 import subprocess
 import sys
 from PIL import Image
-
+import shutil
 def process_image(output_dir, image_path, output_path ,output_name):
     command = [
         sys.executable,
@@ -44,23 +45,24 @@ header_content_end = f"""
 #endif
 """
 
-def main(argv):
-    thisPyDirPath = os.path.dirname(os.path.abspath(__file__))
-    if not argv:
-        current_dir = thisPyDirPath
-    else:
-        current_dir = argv[0]
-    if current_dir[-1] == '\\' or current_dir[-1] == '/':
-        current_dir = current_dir[:-1]
-    print(current_dir)
+def generate_image_data_for_each_config(imageYaml, output_dir):
+    for image_entry in imageYaml:
+        image_path = image_entry.get("path")
+        if image_path:
+            dest_path = os.path.join(output_dir, os.path.basename(image_path))
+
+            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+            shutil.copyfile(image_path, dest_path)
+            thisPyDirPath = os.path.dirname(os.path.abspath(__file__))
+    print(output_dir)
     print('\nfile list:')
     header_content=""
-    for filename in os.listdir(current_dir):
+    for filename in os.listdir(output_dir):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
             base_name, file_extension = os.path.splitext(filename)
-            image_path = os.path.join(current_dir, filename)
+            image_path = os.path.join(output_dir, filename)
             new_filename = f"{base_name}.c"
-            output_path = os.path.join(current_dir, new_filename)
+            output_path = os.path.join(output_dir, new_filename)
             output_name = f'_{filename.replace(".", "_")}_'
             text_file_name=filename.replace(".", "_")
             print('    ',filename,' -> ',text_file_name)
@@ -105,15 +107,31 @@ extern const arm_2d_tile_t c_tile_{text_file_name}_Mask;
                 header_content += f"""
 """
 
-
-            with open(f"{current_dir}/uiImages.h", 'w') as file:
+            with open(f"{output_dir}/uiImages.h", 'w') as file:
                 file.write(header_content_start)
                 file.flush()
                 file.write(header_content)
                 file.flush()
                 file.write(header_content_end)
                 file.flush()
+            os.remove(image_path)
     print('completed\n')
+def main(argv):
+    if not argv:
+        output_dir = os.path.dirname(os.path.abspath(__file__))
+    else:
+        output_dir = argv[0]
+    if output_dir[-1] == '\\' or output_dir[-1] == '/':
+        output_dir = output_dir[:-1]
+
+    image_yaml_path = os.path.join(output_dir, "image.yaml")
+
+    if os.path.exists(image_yaml_path):
+        with open(image_yaml_path, 'r') as file:
+            imageYamlData = yaml.safe_load(file)
+            generate_image_data_for_each_config(imageYamlData, output_dir)
+    else:
+        print("image.yaml not found")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
