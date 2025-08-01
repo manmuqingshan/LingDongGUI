947e663db566c8e85dfafc7b0d648ebbae7b3a39
gzbkey <59935554@qq.com>
2025-08-01 17:19:27 +0800

    1、同步2d 2、图片外置数据和内置一样操作

--- src/gui/ldImage.h
+++ src/gui/ldImage.h
@@ -49,10 +49,6 @@ struct ldImage_t {
     ldColor bgColor;
     arm_2d_tile_t* ptImgTile;
     arm_2d_tile_t* ptMaskTile;
-#if USE_VIRTUAL_RESOURCE == 1
-    arm_2d_vres_t imgTile;
-    arm_2d_vres_t maskTile;
-#endif
     bool isTransparent:1;//window专用
 };
 
